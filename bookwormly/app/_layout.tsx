487cbfd30aa2154d172f8affc488d7351621bed0
aramind <88814622+aramind@users.noreply.github.com>
2025-06-03 11:35:36 +0800

    feat: initial set up for login and signup screens

--- bookwormly/app/_layout.tsx
+++ bookwormly/app/_layout.tsx
@@ -1,10 +1,19 @@
+import SafeScreen from "@/components/SafeScreen";
 import { Stack } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 
 export default function RootLayout() {
   return (
-    <Stack screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="index" options={{ title: "Home" }} />
-      <Stack.Screen name="(auth)" options={{ title: "Home" }} />
-    </Stack>
+    <SafeAreaProvider>
+      <SafeScreen>
+        {" "}
+        <Stack screenOptions={{ headerShown: false }}>
+          <Stack.Screen name="index" options={{ title: "Home" }} />
+          <Stack.Screen name="(auth)" options={{ title: "Home" }} />
+        </Stack>
+      </SafeScreen>
+      <StatusBar style="inverted" />
+    </SafeAreaProvider>
   );
 }
