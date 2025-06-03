3ab05d7d6c2ee6ca7a580548e434e5315e4b5448
aramind <88814622+aramind@users.noreply.github.com>
2025-06-03 16:02:56 +0800

    refactor: combining the extracting from auth store
    - since it only has 3

--- bookwormly/app/(auth)/signup.jsx
+++ bookwormly/app/(auth)/signup.jsx
@@ -13,7 +13,6 @@ import { Ionicons } from "@expo/vector-icons";
 import COLORS from "@/constants/Colors";
 import { Link } from "expo-router";
 import useAuthStore from "@/store/authStore";
-import shallow from "zustand/shallow";
 
 const Signup = () => {
   const [username, setUsername] = useState("");
@@ -21,9 +20,7 @@ const Signup = () => {
   const [password, setPassword] = useState("");
   const [showPassword, setShowPassword] = useState(false);
 
-  const isLoading = useAuthStore((s) => s.isLoading);
-  const signup = useAuthStore((s) => s.signup);
-  const user = useAuthStore((s) => s.user);
+  const { user, isLoading, signup } = useAuthStore();
 
   //   handlers
   const handleSignup = async () => {
