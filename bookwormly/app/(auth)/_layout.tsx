91004edfee6d1d26e22ae9c4853a9868437c1a5e
aramind <88814622+aramind@users.noreply.github.com>
2025-06-13 00:17:54 +0800

    feat: addign image picking functionality

--- bookwormly/app/(auth)/_layout.tsx
+++ bookwormly/app/(auth)/_layout.tsx
@@ -3,6 +3,33 @@ import { Stack, useRouter, useSegments } from "expo-router";
 import useAuthStore from "@/store/authStore";
 
 const AuthLayout = () => {
+  const router = useRouter();
+  const segments = useSegments();
+  const { checkAuth, refreshToken, user } = useAuthStore();
+
+  const [initialCheckDone, setInitialCheckDone] = useState(false);
+
+  useEffect(() => {
+    const verify = async () => {
+      await checkAuth(); // checks and sets `user` and `refreshToken`
+      setInitialCheckDone(true); // now we can safely navigate
+    };
+    verify();
+  }, [checkAuth]);
+
+  console.log("SEGMENTS", segments);
+
+  useEffect(() => {
+    if (!initialCheckDone || !segments) return;
+    const inAuthScreen = segments[0] === "(auth)";
+    const isSignedIn = user && refreshToken;
+
+    if (!isSignedIn && !inAuthScreen) {
+      router.replace("/(auth)");
+    } else if (isSignedIn && inAuthScreen) {
+      router.replace("/(tabs)");
+    }
+  }, [initialCheckDone, segments, refreshToken, user, router]);
   return <Stack screenOptions={{ headerShown: false }} />;
 };
 
--- bookwormly/app/(tabs)/create.jsx
+++ bookwormly/app/(tabs)/create.jsx
@@ -7,11 +7,14 @@ import {
   Platform,
   ScrollView,
   TouchableOpacity,
+  Image,
+  Alert,
 } from "react-native";
 import styles from "@/assets/styles/styles.general";
 import { Ionicons } from "@expo/vector-icons";
 import COLORS from "@/constants/Colors";
 import { TextInput } from "react-native-gesture-handler";
+import * as ImagePicker from "expo-image-picker";
 
 const Create = () => {
   const [title, setTitle] = useState("");
@@ -23,7 +26,38 @@ const Create = () => {
 
   const router = useRouter();
 
-  const pickImage = async () => {};
+  const pickImage = async () => {
+    try {
+      // request permission if needed
+      if (Platform.OS !== "web") {
+        const { status } =
+          await ImagePicker.requestMediaLibraryPermissionsAsync();
+        if (status !== "granted") {
+          Alert.alert(
+            "Permission Denied",
+            "We need camera roll permissions to upload an image"
+          );
+          return;
+        }
+
+        // launch image library
+        const result = await ImagePicker.launchImageLibraryAsync({
+          mediaTypes: "images",
+          allowsEditing: true,
+          aspect: [4, 3],
+          quality: 0.5, //lower the quality for smaller base64
+          base64: true,
+        });
+
+        if (!result.canceled) {
+          console.log(result);
+          setImage(result.assets[0].uri);
+        }
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
   const handleSubmit = async () => {};
 
   const renderRatingPicker = () => {
@@ -87,6 +121,27 @@ const Create = () => {
           <View style={styles.formGroup}>
             <Text style={styles.label}>Your Rating</Text>
             {renderRatingPicker()}
+          </View>
+
+          {/* IMAGE */}
+          <View style={styles.formGroup}>
+            <Text style={styles.label}>Book Image</Text>
+            <TouchableOpacity style={styles.imagePicker} onPress={pickImage}>
+              {image ? (
+                <Image source={{ uri: image }} style={styles.previewImage} />
+              ) : (
+                <View style={styles.placeholderContainer}>
+                  <Ionicons
+                    name="image-outline"
+                    size={40}
+                    color={COLORS.textSecondary}
+                  />
+                  <Text style={styles.placeholderText}>
+                    Tap to select image
+                  </Text>
+                </View>
+              )}
+            </TouchableOpacity>
           </View>
         </View>
       </ScrollView>
